bd451d5cc6e29ed56f10eb256b2a66a76978333b
erennkazan@gmail.com <erennkazan@gmail.com>
2023-07-08 02:13:12 +0300

    fixed

--- deneme/src/App.js
+++ deneme/src/App.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
+import Course from './Course';
 
+function getRandomCourse(){
+  const courseArray = ['Angular','Bootstrap','Ccsharp','KompleWeb'];
+  return courseArray[Math.floor(Math.random()*courseArray.length)];
+}
 function App() {
-  
+  const [courses, setcourses] = useState([])
+
+  const handleClick = () =>{
+    setcourses([...courses, getRandomCourse()])
+  };
+
+  const courseList = () =>{
+    
+  };
 
   return (
     <div className="App">
+      <button onClick={handleClick}>Kurs Ekle</button>
+      {courses.map((course,index)=>{
+        return <Course key={index} courseName={course}/>
+      })  
+      }
      
     </div>
   );
---
+++ deneme/src/Course.js
@@ -0,0 +1,15 @@
+import Angular from './images/angular.jpg';
+import Bootstrap from './images/bootstrap5.png';
+import Ccsharp from './images/ccsharp.png';
+import Kompleweb from './images/kompleweb.jpg';
+
+function Course({courseName}) {
+                    console.log(courseName);
+     return ( 
+  <div>
+      <img src={Angular} />
+  </div>
+    );                                                                              
+}
+
+export default Course;
