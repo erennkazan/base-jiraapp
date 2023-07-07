e203f11b430aa5c415182c65fd5f7798807164a7
erennkazan@gmail.com <erennkazan@gmail.com>
2023-07-08 02:28:04 +0300

    fixed

--- deneme/src/App.js
+++ deneme/src/App.js
@@ -12,18 +12,14 @@ function App() {
     setcourses([...courses, getRandomCourse()])
   };
 
-  const courseList = () =>{
-    
-  };
+  const courseList =  courses.map((course,index)=>{
+    return <Course key={index} courseName={course}/>
+  });
 
   return (
     <div className="App">
       <button onClick={handleClick}>Kurs Ekle</button>
-      {courses.map((course,index)=>{
-        return <Course key={index} courseName={course}/>
-      })  
-      }
-     
+      <div>{courseList}</div>
     </div>
   );
 }
--- deneme/src/Course.js
+++ deneme/src/Course.js
@@ -3,11 +3,20 @@ import Bootstrap from './images/bootstrap5.png';
 import Ccsharp from './images/ccsharp.png';
 import Kompleweb from './images/kompleweb.jpg';
 
+const courseMap = {
+    Angular,
+    Bootstrap,
+    Ccsharp,
+    Kompleweb
+}
+
 function Course({courseName}) {
-                    console.log(courseName);
+//      console.log(courseName);
+   console.log(courseMap[courseName]);   
+     
      return ( 
   <div>
-      <img src={Angular} />
+      <img src={courseMap[courseName]} alt="course"/>
   </div>
     );                                                                              
 }
