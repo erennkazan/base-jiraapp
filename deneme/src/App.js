b3252c738b422f1a835125f4987a8585d7af9511
erennkazan@gmail.com <erennkazan@gmail.com>
2023-07-08 01:51:58 +0300

    fixed

--- deneme/src/App.js
+++ deneme/src/App.js
@@ -1,22 +1,11 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [displayText, setDisplayText] = useState('');
-
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleClick = () => {
-    setDisplayText(inputValue);
-  };
+  
 
   return (
     <div className="App">
-      <input type="text" value={inputValue} onChange={handleChange} />
-      <button onClick={handleClick}>Göster</button>
-      <p>{displayText}</p>
+     
     </div>
   );
 }
