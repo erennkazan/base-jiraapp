210b37e5628536e6e9a0847cc716962e80785cfd
erennkazan@gmail.com <erennkazan@gmail.com>
2023-07-09 00:54:12 +0300

    task show created

--- deneme/src/App.js
+++ deneme/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
-import {useState} from 'react'
-import TaskCreate from './components/TaskCreate'
-import TaskList from './components/TaskList'
+import TaskCreate from './components/TaskCreate';
+import TaskList from './components/TaskList';
+import { useState } from 'react';
 
-function App(){
-
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const createTask = (title, taskDesc) => {
+    const createdTasks = [
+      ...tasks,
+      {
+        id: Math.round(Math.random() * 999999),
+        title,
+        taskDesc,
+      },
+    ];
+    setTasks(createdTasks);
+  };
   return (
     <div className="App">
-      <TaskCreate/>
+      <TaskCreate onCreate={createTask} />
       <h1>Görevler</h1>
-      <TaskList/>
+      <TaskList tasks={tasks} />
     </div>
   );
 }
--- deneme/src/components/TaskCreate.js
+++ deneme/src/components/TaskCreate.js
@@ -1,31 +1,41 @@
-import {useState} from 'react'
+import { useState } from 'react';
 
-function TaskCreate() {
-const [title, setTitle] = useState('');
-const [taskDesc, setTaskDesc] = useState('');
-console.log(title,taskDesc);
+function TaskCreate({ onCreate }) {
+  const [title, setTitle] = useState('');
+  const [taskDesc, setTaskDesc] = useState('');
 
-const handleChange = (event) => {
+  const handleChange = (event) => {
     setTitle(event.target.value);
-}
-const handleTaskChange = (event) => {
+  };
+  const handleTaskChange = (event) => {
     setTaskDesc(event.target.value);
-}
-const handleSubmit = (event) => { 
+  };
+  const handleSubmit = (event) => {
     event.preventDefault();
-}
+    onCreate(title, taskDesc);
+    setTitle('');
+    setTaskDesc('');
+  };
 
-   return (
-   <div className="task-create">
-    <h3>Lütfen Task Ekleyiniz</h3>
-    <form className="task-form">
-       <label className="task-label">Başlık</label>
-       <input value={title} onChange={handleChange} className="task-input"/>
-       <label className="task-label">Task Giriniz!</label>
-       <textarea value={taskDesc} onChange={handleTaskChange} className="task-input" rows={5}/>
-       <button className="task-button"onClick={handleSubmit}>Oluştur</button>
-    </form>
-   </div>  );
+  return (
+    <div className="task-create">
+      <h3>Lütfen Task Ekleyiniz!</h3>
+      <form className="task-form">
+        <label className="task-label">Başlık</label>
+        <input value={title} onChange={handleChange} className="task-input" />
+        <label className="task-label">Task Giriniz!</label>
+        <textarea
+          value={taskDesc}
+          onChange={handleTaskChange}
+          className="task-input"
+          rows={5}
+        />
+        <button className="task-button" onClick={handleSubmit}>
+          Oluştur
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default TaskCreate;
