6d297475b7648c6c8db0cefb8f451d0f1bc6b852
erennkazan@gmail.com <erennkazan@gmail.com>
2023-07-08 22:58:52 +0300

    task add project created

--- deneme/src/App.js
+++ deneme/src/App.js
@@ -1,26 +1,15 @@
-import React, { useState } from 'react';
-import Course from './Course';
 import './App.css';
+import {useState} from 'react'
+import TaskCreate from './components/TaskCreate'
+import TaskList from './components/TaskList'
 
-function getRandomCourse(){
-  const courseArray = ['Angular','Bootstrap','Ccsharp','KompleWeb'];
-  return courseArray[Math.floor(Math.random()*courseArray.length)];
-}
-function App() {
-  const [courses, setcourses] = useState([])
-
-  const handleClick = () =>{
-    setcourses([...courses, getRandomCourse()])
-  };
-
-  const courseList =  courses.map((course,index)=>{
-    return <Course key={index} courseName={course}/>
-  });
+function App(){
 
   return (
     <div className="App">
-      <button className="appButton" onClick={handleClick}>Kurs Ekle</button>
-      <div className="courseList">{courseList}</div>
+      <TaskCreate/>
+      <h1>Görevler</h1>
+      <TaskList/>
     </div>
   );
 }
